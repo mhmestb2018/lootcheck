90b75f38b982cd6ac448ea58839789ee6b6a9ce1
Mona Temchin <monatemchin@gmail.com>
2019-01-01 20:05:14 -0500

    Installed and configured Adapter for enzyme for React 16

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
 
 describe('App',() => {
   const app = shallow(<App />);
