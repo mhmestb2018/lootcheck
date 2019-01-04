31ae9307bd210b1e00dc6a9c616c51601fe53d74
Mona Temchin <monatemchin@gmail.com>
2019-01-03 19:13:54 -0500

    Connect wallet component in app

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -13,4 +13,8 @@ describe('App',() => {
   expect(app).toMatchSnapshot();
   });
 
+  it('contains a connected Wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true);
+  });
+
 });
--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-class Wallet extends Component {
+export class Wallet extends Component {
   render() {
     return (
       <div>
-       <h3>Wallet balance</h3>
+       <h3 className='balance'>Wallet balance: {this.props.balance}</h3>
       </div>
     )
   }
 }
 
-export default connect()(Wallet);
+export default connect(state => { balance: state }, null)(Wallet);
