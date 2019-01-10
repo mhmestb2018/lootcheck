bb7f2f1097f126c497ae2f8a1975376eec833cc8
Mona Temchin <monatemchin@gmail.com>
2019-01-10 17:31:38 -0500

    Fixed type error in mount test by adding mock function to props

--- src/components/Loot.js
+++ src/components/Loot.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { fetchBitcoin } from '../actions/bitcoin';
 
 export class Loot extends Component {
+
   componentDidMount() {
     this.props.fetchBitcoin();
   }
 
   render() {
     return (
-      <h3>Bitcoin Balance</h3>
+      <h3>Bitcoin balance:</h3>
     )
   }
 }
--- src/components/Loot.test.js
+++ src/components/Loot.test.js
@@ -6,15 +6,15 @@ import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
 describe('Loot', () => {
-  let props = { balance: 10, bitcoin: {} };
-  let loot = shallow(<Loot {...props}/>);
+  const mockFetchBitcoin = jest.fn();
+  let props = { balance: 10, bitcoin: {}, fetchBitcoin: mockFetchBitcoin };
+  let loot = shallow(<Loot {...props} />);
 
   it('renders properly', () => {
      expect(loot).toMatchSnapshot();
   });
 
   describe('when mounted', () => {
-    const mockFetchBitcoin = jest.fn();
 
     beforeEach(() => {
       props.fetchBitcoin = mockFetchBitcoin;
